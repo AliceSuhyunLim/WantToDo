5e46f342f5be41eec18cae4162d2356093829966
임수현 <alice_lim@imsuhyeon-ui-MacBookPro.local>
2020-03-08 22:11:35 +0900

    App.js

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions} from 'react-native';
+
+const {height, width} = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content"/>
+      <Text style={styles.title}>WantToDo</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New To do"}/>
+      </View>
+
     </View>
   );
 }
@@ -12,8 +19,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f23657',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    color: 'white',
+    fontSize: 20,
+    marginTop: 50,
+    marginBottom: 50,
+    fontWeight: '600'
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
   },
 });
